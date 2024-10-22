640da7d0582dea15e8857c3cabad2e90f34cf782
Daniel Diaz <daniel_diaz@192.168.0.102>
2024-10-22 16:15:51 -0500

    documentation added

--- cdk-stori/cdk_stori/cdk_stori_stack.py
+++ cdk-stori/cdk_stori/cdk_stori_stack.py
@@ -8,15 +8,30 @@ from aws_cdk import (
 )
 
 from constructs import Construct
-from pathlib import Path
 
 class TumbnailGeneratorStack(Stack):
+    """
+    Stack responsible for setting up AWS resources required for the Thumbnail Generator service, 
+    including an AWS Lambda function, S3 buckets for source and target images, 
+    and a DynamoDB table for storing image metadata.
+    """
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+        """
+        Initializes the Thumbnail Generator stack with the necessary AWS resources.
+
+        Parameters:
+        - scope (Construct): Scope in which this stack is defined.
+        - construct_id (str): Identifier for this construct.
+        - kwargs (dict): Additional keyword arguments.
+
+        Returns:
+        None
+        """
+        super().__init__(scope, construct_id, **kwargs)
         self.construct_id = construct_id
 
-        #Main methods
+        # Main methods
         self.create_lambda_function()
         self.create_source_s3_bucket()
         self.create_target_s3_bucket()
@@ -24,43 +39,79 @@ class TumbnailGeneratorStack(Stack):
         self.create_dynamo_db_table()
 
     def create_lambda_function(self):
+        """
+        Creates an AWS Lambda function that will generate thumbnails from source images.
+
+        The Lambda function uses Python 3.8 runtime and has a 15-second timeout with 128 MB memory.
+
+        Returns:
+        None
+        """
         self.fn = _lambda.Function(
                             self,
-                            function_name = "thumbnails-generator",
-                            description = "This Lambda function is a Thumbnail generator",
-                            runtime = _lambda.Runtime.PYTHON_3_8,
-                            handler = "lambda_function.lambda_handler",
-                            code = _lambda.Code.from_asset("./lambda"),
-                            timeout = Duration.seconds(15),
+                            function_name="thumbnails-generator",
+                            description="This Lambda function is a Thumbnail generator",
+                            runtime=_lambda.Runtime.PYTHON_3_8,
+                            handler="lambda_function.lambda_handler",
+                            code=_lambda.Code.from_asset("./lambda"),
+                            timeout=Duration.seconds(15),
                             memory_size=128)
-    
+
     def create_source_s3_bucket(self):
+        """
+        Creates an S3 bucket to store the source images.
+
+        The bucket is versioned, has public access blocked, uses S3 managed encryption,
+        and requires SSL for communication.
+
+        Returns:
+        None
+        """
         self.source_bucket = s3.Bucket(
                                     self,
-                                    bucket_name = "thumbnails-generator-source-images-bucket",
-                                    block_public_access = s3.BlockPublicAccess.BLOCK_ALL,
+                                    bucket_name="thumbnails-generator-source-images-bucket",
+                                    block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
                                     encryption=s3.BucketEncryption.S3_MANAGED,
                                     enforce_sSL=True,
                                     versioned=True)
 
-    
     def create_target_s3_bucket(self):
+        """
+        Creates an S3 bucket to store the generated thumbnail images.
+
+        Like the source bucket, this bucket is versioned, has public access blocked,
+        and uses S3 managed encryption with SSL enforcement.
+
+        Returns:
+        None
+        """
         self.target_bucket = s3.Bucket(
                                     self, 
-                                    bucket_name = "thumbnails-generator-target-images-bucket",
-                                    block_public_access = s3.BlockPublicAccess.BLOCK_ALL,
+                                    bucket_name="thumbnails-generator-target-images-bucket",
+                                    block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
                                     encryption=s3.BucketEncryption.S3_MANAGED,
                                     enforce_sSL=True,
                                     versioned=True)
 
     def create_s3_event(self):
-        # create s3 notification for lambda function
+        """
+        Configures an S3 event to trigger the Lambda function whenever a new object 
+        (image) is uploaded to the source S3 bucket.
+
+        Returns:
+        None
+        """
         self.notification = aws_s3_notifications.LambdaDestination(self.fn)
-        # assign notification for the s3 event type
         self.source_bucket.add_event_notification(s3.EventType.OBJECT_CREATED, self.notification)
-    
+
     def create_dynamo_db_table(self):
-        # Create Dynamodb table to store image's metadata
+        """
+        Creates a DynamoDB table to store metadata about the images processed by the Lambda function.
+
+        The table uses a string partition key named 'id'.
+
+        Returns:
+        None
+        """
         self.metadata_table = dynamodb.Table(self, "thumbails-generator-metadata",
-            partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING)
-        )
+            partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING))
