f8a2c476509442bdd31e80856f7b4ea9fad30c60
Daniel Diaz <daniel_diaz@192.168.0.102>
2024-10-22 14:48:20 -0500

    stack created

--- cdk-stori/cdk_stori/cdk_stori_stack.py
+++ cdk-stori/cdk_stori/cdk_stori_stack.py
@@ -1,19 +1,43 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_lambda_event_sources as lambda_events,
+    aws_s3 as s3,
+    aws_dynamodb as dynamodb,
+    aws_s3_notifications
 )
+
 from constructs import Construct
+from pathlib import Path
 
-class CdkStoriStack(Stack):
+class TumbnailGeneratorStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        self.create_lambda_function()
+        self.create_s3_bucket("thumbnails-generator-source-images-bucket")
+        self.create_s3_bucket("thumbnails-generator-target-images-bucket")
+        self.create_s3_event()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStoriQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        def create_lambda_function(self):
+            self.fn = _lambda.Function(
+                                self,
+                                function_name = "thumbnails-generator",
+                                description = "This Lambda function is a Thumbnail generator",
+                                runtime = _lambda.Runtime.PYTHON_3_8,
+                                handler = "lambda_function.lambda_handler",
+                                code = _lambda.Code.from_asset("./lambda"),
+                                timeout = Duration.seconds(15),
+                                memory_size=128)
+        
+        def create_s3_bucket(self, bucket_name):
+            # Create soruce S3 bucket (original images)
+            self.source_bucket = s3.Bucket(self, bucket_name)
+        
+        def create_s3_event(self):
+            # create s3 notification for lambda function
+            notification = aws_s3_notifications.LambdaDestination(function)
+            # assign notification for the s3 event type
+            s3.add_event_notification(s3.EventType.OBJECT_CREATED, notification)
